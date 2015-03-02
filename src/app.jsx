9616dff648b1bbb888cae5f2ce85c9775200c079
allensunbo <allensunbo@gmail.com>
2015-03-02 17:17:13 -0500

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+/**
+ * Created by bsun on 3/2/15.
+ */
+var React = require("react");
+
+var App = React.createClass({
+   render() {
+      return <h1>Hello world!</h1>
+   }
+});
+
+React.render(<App/>, document.getElementById("example"));
+
+
