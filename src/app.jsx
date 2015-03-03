535b4b91d79da14a2b7958796a7c90b4f9521ebb
allensunbo <allensunbo@gmail.com>
2015-03-03 16:07:55 -0500

    refactor

--- src/app.jsx
+++ src/app.jsx
@@ -1,39 +1,52 @@
-var React = require("react");
+(function () {
+   var React = require("react");
 
-var App = React.createClass({
-   getInitialState: function () {
-      return {
-         speed: undefined,
-         degree: undefined
-      };
-   },
-   getData: getData,
-   render() {
-      return <div>
-         <button onClick={this.getData}>Get Data</button>
-         <p>wind speed = {this.state.speed}</p>
-         <p>wind degree = {this.state.degree}</p>
-      </div>
-   }
-});
 
-React.render(<App/>, document.getElementById("example"));
-
-function getData() {
-   var request = new XMLHttpRequest(), self = this;
-   request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk', true);
-   request.onload = function () {
-      if (request.status >= 200 && request.status < 400) {
-         // console.log(request.responseText);
-         var r = JSON.parse(request.responseText);
-         self.setState({
-            speed: r.wind.speed,
-            degree: r.wind.deg
-         });
-      } else {
-         console.log('error getting weather data');
+   var App = React.createClass({
+      getInitialState: function () {
+         return {
+            speed: undefined,
+            degree: undefined
+         };
+      },
+      getData: getData,
+      render() {
+         return <div>
+            <button onClick={this.getData}>Get Data</button>
+            <p>wind speed = {this.state.speed}</p>
+            <p>wind degree = {this.state.degree}</p>
+         </div>
       }
-   };
-   request.send();
-}
+   });
 
+   React.render(<App/>, document.getElementById("example"));
+
+   function getData() {
+      var self = this;
+      console.log(self);
+      // example of using bind here
+      var callback = function (request, scope) {
+         if (request.status >= 200 && request.status < 400) {
+            // console.log(request.responseText);
+            var r = JSON.parse(request.responseText);
+            scope.setState({
+               speed: r.wind.speed,
+               degree: r.wind.deg
+            });
+         } else {
+            console.log('error getting weather data');
+         }
+      };
+      var doGetBind = doGet.bind(self);
+      doGetBind('http://api.openweathermap.org/data/2.5/weather?q=London,uk', callback);
+   }
+
+   function doGet(url, callback) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.onload = callback.bind(undefined, request, this);
+      request.send();
+      console.log(this);
+   }
+
+})();
