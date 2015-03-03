4d53ba566cf34b8acb1eae78cd32c018b3923211
allensunbo <allensunbo@gmail.com>
2015-03-02 19:46:59 -0500

    add http request

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,39 @@
-/**
- * Created by bsun on 3/2/15.
- */
 var React = require("react");
 
 var App = React.createClass({
+   getInitialState: function () {
+      return {
+         speed: 0,
+         degree: 0
+      };
+   },
+
+   getData: function () {
+      var self = this;
+      var request = new XMLHttpRequest(), self = this;
+      request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk', true);
+      request.onload = function () {
+         if (request.status >= 200 && request.status < 400) {
+            console.log(request.responseText);
+            var r = JSON.parse(request.responseText);
+            self.setState({
+               speed: r.wind.speed,
+               degree: r.wind.deg
+            });
+         } else {
+            console.log('error getting weather data');
+         }
+      };
+
+      // Fire!
+      request.send();
+   },
    render() {
-      return <h1>Hello world!</h1>
+      return <div>
+         <button onClick={this.getData}>Get Data</button>
+         <p>wind speed = {this.state.speed}</p>
+         <p>wind degree = {this.state.degree}</p>
+      </div>
    }
 });
 
